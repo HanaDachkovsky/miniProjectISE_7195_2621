a6227b29eeefc30d4c03bbc89fcef73683f27af3
שרה תמר אמיתי <saratama@g.jct.ac.il>
2021-05-09 11:38:44 +0300

    final commit 2 ex06

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -19,7 +19,7 @@ public class RenderTests {
 			.setViewPlaneSize(500, 500);
 
 	/**
-	 * Produce a scene with basic 3D model and render it into a jpeg image with a
+	 * Produce a scene with basic 3D model and render it into a png image with a
 	 * grid
 	 */
 	@Test
@@ -48,7 +48,7 @@ public class RenderTests {
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
 	}
-//
+
 //	/**
 //	 * Test for XML based scene - for bonus
 //	 */
@@ -61,7 +61,6 @@ public class RenderTests {
 //		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
 //		Render render = new Render() //
 //				.setImageWriter(imageWriter) //
-//				.setScene(scene) //
 //				.setCamera(camera) //
 //				.setRayTracer(new RayTracerBasic(scene));
 //
@@ -70,11 +69,18 @@ public class RenderTests {
 //		render.writeToImage();
 //	}
 
+	// For stage 6 - please disregard in stage 5
+	/**
+	 * Produce a scene with basic 3D model - including individual lights of the bodies 
+	 * and render it into a png image with a grid
+	 */
 	@Test
 	public void basicRenderMultiColorTest() {
 		Scene scene = new Scene("Test scene")//
 				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
-		scene.geometries.add(new Sphere( new Point3D(0, 0, -100),50), //
+
+		scene.geometries.add(new Sphere( new Point3D(0, 0, -100),50) //
+				.setEmission(new Color(java.awt.Color.CYAN)), //
 				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
 						.setEmission(new Color(java.awt.Color.GREEN)),
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
